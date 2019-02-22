6c61958ecf1d480b3099689d567ddb533d905b90
Wenqi <vickey9.lin@gmail.com>
2019-02-22 15:34:20 +0100

    add: fetch data button

--- newapartment/lib/main.dart
+++ newapartment/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,10 +14,28 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title:Text('Nybolig'),),
+        bottomNavigationBar:Padding(
+          padding:const EdgeInsets.all(8.0),
+          child:RaisedButton(
+            child: new Text("Fetch Data"),
+            onPressed:  () => null,
+          ),
+        ),
         body: Center(child: Text('text'))
       )
     );
   }
 }
 
+class MainFetchData extends StatefulWidget{
+  @override
+  _MainFetchDataState createState() => _MainFetchDataState();
+}
+
+class _MainFetchDataState extends State<MainFetchData>{
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
 
